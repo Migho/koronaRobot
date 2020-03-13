4fe00ceef7f8c22c50c2655cb6a9388c3b4fd52b
Mikko Rinta-Homi <mikkojrh@gmail.com>
2020-03-13 19:19:52 +0200

    Added cron job

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
+const schedule = require('node-schedule');
 console.log("key:", process.env.TELEGRAM_API);
 const Telegraf = require('telegraf')
 const axios = require('axios');
 
 const url = "https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData";
+const subscribers = []
 
 const parseResponse = (resp) => {
     const confirmed = JSON.stringify(resp.data.confirmed.slice(0,5));
@@ -29,9 +31,23 @@ const casesPerCountry = (confirmed) => {
     return JSON.stringify(countries);
 };
 
+// Crons
+schedule.scheduleJob('0 0 10 * * *', () => {
+    axios.get(url).then(resp => {
+        subscribers.forEach(chatId => bot.telegram.sendMessage(chatId, parseResponse(resp)))
+    }).catch(err => {
+        console.error("ERROR:", err);
+    });
+})
+
 const bot = new Telegraf(process.env.TELEGRAM_API)
 
 bot.start((ctx) => {
+    subscribers.push(ctx.chat.id)
+    ctx.reply("Subscribed to Korona news!");
+})
+
+bot.command('stats', (ctx) => {
     axios.get(url).then(resp => {
         ctx.reply(parseResponse(resp));
     }).catch(err => {
