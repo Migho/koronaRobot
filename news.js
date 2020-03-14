3723a65b806a32d65b3414c1c819fac9a53be687
Joonas Sarapalo <joonas.sarapalo@futurice.com>
2020-03-14 16:11:26 +0200

    Block the same news item being sent again when it gets updated in the source

--- news.js
+++ news.js
@@ -1,18 +1,29 @@
 const Parser = require('rss-parser');
 const parser = new Parser();
-
-let newest;
+const oldNews = new Set();
+const ONE_MINUTE_IN_MS = 60000;
 
 const getNews = async (useTimer) => {
     const feed = await parser.parseURL('https://www.hs.fi/rss/tuoreimmat.xml');
+    const now = new Date().getTime();
     const news = [];
     
     feed.items.forEach(item => {
-        const now = new Date();
-        const timeBool = useTimer ? (now.getTime() - Date.parse(item.pubDate)) < 60000 : true;
+        const isCoronaNews = item.title.includes("korona") ||
+            item.title.includes("tartunt") ||
+            item.title.includes("Korona") ||
+            item.title.includes("Tartunt");
+        const correctTime = useTimer ? (now - Date.parse(item.pubDate)) < ONE_MINUTE_IN_MS : true;
+        const isNotOldNews = !oldNews.has(item.guid);
 
-        if (item.title.includes("korona", "tartunt") && timeBool) {
-            news.push(item.title.split("|")[1].trim() + ': ' + item.link + "\n");
+        if (isCoronaNews && correctTime && isNotOldNews) {
+            const splitTitle = item.title.split("|").length;
+            if (splitTitle.length == 2) {
+                news.push(splitTitle[1].trim() + ': ' + item.link + "\n");
+            } else {
+                news.push(item.title.trim() + ': ' + item.link + "\n");
+            }
+            oldNews.add(item.guid);
         }
     });
 
